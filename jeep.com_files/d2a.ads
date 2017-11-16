ddf6a0615954ba930d7e5671b277a2d20c4ca8da
Mathias Siemer <mathiassiemer@MathiassMacBook.attlocal.net>
2017-11-15 21:54:58 -0600

    commit

---
+++ jeep.com_files/d2a.ads
@@ -0,0 +1,25 @@
+/*
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+*/
+(function(){var b={d:"",a:function(){if(b.d=="1"){var a=document.createElement("iframe"),c=document.getElementsByTagName("script")[0],e="https://a01.korrelate.net/a/e/d2r.ads?et=r&adsuu=XjFuA7G4flQj&"+(new Date).getTime();a.width=0;a.height=0;a.frameBorder=0;a.border=0;c.parentNode.insertBefore(a,c);setTimeout(function(){a.src=e},1)}}},d={c:"",b:function(){if(d.c!="oo")(new Image).src="https://tapestry.tapad.com/tapestry/1?ta_partner_id=1866&ta_format=png&ta_partner_did=XjFuA7G4flQj"}};
+d.b();b.a()})();
